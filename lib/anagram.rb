e443501f6a634128b80917842060c6ef8e4a05d4
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 16:23:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize (word)
+    @word=word
+  end
+
+  def match(sentenece)
+    arr=Array.new
+    arr=sentenece.split(" ")
+    arr2=Array.new
+    arr.each do |w|
+      if @word.sort==w.sort
+        arr2<<w
+      end
+    end
+    return arr2
+  end
+  
+end
